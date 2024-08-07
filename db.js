de48f171b7caa51b378cf2e153faad2e26713411
unknown <mwaqas3030@gmail.com>
2024-08-07 20:49:00 +0500

    asdf

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
 mongoose
-  .connect("mongodb+srv://bughlani:bughlani@cluster0.9qki3lg.mongodb.net/tamam?retryWrites=true&w=majority&appName=Cluster0")
+  .connect(process.env.mongoDBStr)
   .then(() => console.log("Database connected"))
   .catch((err) => console.log(`DB connection failed ${err}`));
